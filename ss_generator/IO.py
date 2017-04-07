18ed7f606c947cce0b748200d24559d31e66fca7
xingjiepan <xingjiepan@gmail.com>
2017-04-07 12:35:34 -0700

    Update the save_ca_list() function.

--- ss_generator/IO.py
+++ ss_generator/IO.py
@@ -1,8 +1,25 @@
+import numpy as np
+
+
 def save_ca_list(ca_list, pdb_name):
-    '''Save a list of Ca atoms into a PDB file'''
+    '''Save a Ca atoms into a PDB file.
+    The input list can either be a list of coordinates, or
+    a list of list.
+    '''
+    # Pack the single level list input
+
+    if isinstance(ca_list[0], np.ndarray):
+        ca_list = [ca_list]
+
     with open(pdb_name, 'w') as f:
-        for i, ca in enumerate(ca_list):
+        index = 0
         
-            f.write("ATOM  {0:5d}   CA ALA A".format(i + 1) \
-                    + "{0:4d}    {1:8.3f}{2:8.3f}{3:8.3f}".format(i + 1, ca[0], ca[1], ca[2]) \
-                    + "                       C  \n")
+        for l in ca_list:
+            for ca in l:
+            
+                f.write("ATOM  {0:5d}   CA ALA A".format(index) \
+                        + "{0:4d}    {1:8.3f}{2:8.3f}{3:8.3f}".format(index, ca[0], ca[1], ca[2]) \
+                        + "                       C  \n")
+                index += 1
+
+            f.write("TER\n")
