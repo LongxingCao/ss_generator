43dabd4394752b3229b39718444bc287eb02c621
xingjiepan <xingjiepan@gmail.com>
2017-04-17 19:15:18 -0700

    Reimplement get_internal_coordinates_for_ideal_strand() and get_ideal_parameters_from_internal_coordinates()

--- ss_generator/beta_sheet.py
+++ ss_generator/beta_sheet.py
@@ -8,11 +8,13 @@ from . import basic
 D_MEAN = 3.80
 PARA_BP_LEN_PLUS = 4.84
 
-def get_internal_coordinates_for_ideal_sheet(R, alpha, delta):
-    '''Get 4 internal coordinates for an ideal beta sheet.
+def get_internal_coordinates_for_ideal_strand(R, delta, alpha, eta):
+    '''Get 4 internal coordinates for an ideal beta strand.
     The inputs are the screw redius R, the angle alpha which is
     between a tangent of the screw spiral and the horizontal plane
-    and the screw angle omega between Ca_i and Ca_i+2.
+    and the screw angle omega between Ca_i and Ca_i+2, delta which
+    is the screw angle from Ca_i to Ca_i+2 and eta which is the value
+    of tilting.
 
     Note that alpha is in the range (0, pi/2) for right handed strands
     and (pi/2, pi) for left handed strands.
@@ -25,119 +27,127 @@ def get_internal_coordinates_for_ideal_sheet(R, alpha, delta):
     if alpha > np.pi / 2:
         delta = -delta
 
-    theta1 = 2 * np.arcsin(R * np.absolute(np.sin(delta / 2)) / (D_MEAN * np.absolute(np.cos(alpha))))
+    theta1 = 2 * np.arcsin(R * np.absolute(np.tan(delta / 2)) / (D_MEAN * np.absolute(np.cos(alpha))))
     h = 2 * D_MEAN * np.sin(theta1 / 2) * np.sin(alpha)
 
-    p1 = np.array([D_MEAN * np.cos(theta1 / 2), 0, 0])
     p2 = np.array([0, D_MEAN * np.sin(theta1 / 2) * np.cos(alpha), h / 2])
-    q = np.array([-D_MEAN * np.sin(theta1 / 2) * np.cos(alpha) * np.sin(delta),
-        D_MEAN * np.sin(theta1 / 2) * np.cos(alpha) * (1 + np.cos(delta)), h])
-    p3 = q + D_MEAN * np.cos(theta1 / 2) * np.array([np.cos(delta), np.sin(delta), 0])
+
+    # Get p1
+
+    p1_0 = np.array([D_MEAN * np.cos(theta1 / 2), 0, 0])
+    M1 = geometry.rotation_matrix_from_axis_and_angle(p2, eta)
+    p1 = np.dot(M1, p1_0)
+
+    # Get p3 and p4
+
+    M_screw, t_screw = geometry.get_screw(np.array([0, 0, 1]), delta,
+            2 * np.pi / np.absolute(delta) * h, np.array([-R, 0, 0]))
+
+    p3 = np.dot(M_screw, p1) + t_screw
+    p4 = np.dot(M_screw, p2) + t_screw
 
     theta2 = geometry.angle(p1 - p2, p3 - p2)
     tau1 = geometry.dihedral(-p2, p1, p2, p3)
-    tau2 = geometry.dihedral(p1, p2, p3, 2 * q - p2)
+    tau2 = geometry.dihedral(p1, p2, p3, p4)
 
     return theta1, tau1, theta2, tau2
 
-def get_ideal_parameters_from_three_internal_coordinates(theta1, tau1, theta2):
-    '''Get 3 ideal beta sheet parameters R, alpha and delta from
-    three internal coordinates
-    '''
-    if theta1 > theta2:
-        raise Exception("Current implementation requires theta1 < theta2.")
+def get_ideal_parameters_from_internal_coordinates(theta1, tau1, theta2, tau2):
+    '''Get ideal parameters R, delta, alpha and eta from internal coordinates.'''
+
+    if theta2 < theta1:
+        raise Exception("Current implementation requires theta2 >= theta1.")
+
+    # Get all the points
 
     p0 = D_MEAN * np.array([np.sin(theta1), np.cos(theta1), 0])
     p1 = np.array([0, 0, 0])
     p2 = np.array([0, D_MEAN, 0])
-    p3 = D_MEAN * np.array([np.sin(theta2) * np.cos(tau1),
-        1 - np.cos(theta2), -np.sin(theta2) * np.sin(tau1)])
+    p3 = geometry.cartesian_coord_from_internal_coord(p0, p1, p2, D_MEAN, theta2, tau1)
+    p4 = geometry.cartesian_coord_from_internal_coord(p1, p2, p3, D_MEAN, theta1, tau2)
 
-    # Get the screw axis when the strand is in a plane
+    # Get the rotation and translation of the middle point between p0 and p2
 
-    s = np.array([0, 0, 1])
-
-    # Get the screw axis in nondegenerative cases
+    M = np.transpose(geometry.create_frame_from_three_points(p2, p3, p4))
+    t = (p4 - p0) / 2
     
-    if np.absolute(p3[2]) > 0.001:
+    # Get parameters
+    
+    v, delta = geometry.rotation_matrix_to_axis_and_angle(M)
+    if delta < 0:
+        delta = -delta
+        v = -v
 
-        lam = np.dot(2 * p2 - p3, p2 - p0) / np.dot(p3, np.array([0, 0, 1]))
+    R = np.linalg.norm(t - np.dot(t, v) * v) / (2 * np.sin(delta / 2))
+    alpha = np.pi / 2 - geometry.angle(v, p2 - p0)
+    if alpha < 0:
+        alpha += np.pi
 
-        s = geometry.normalize(p2 - p0 + lam * np.array([0, 0, 1]))
+    p1_0 = np.cross(p2 - p0, v)
+    if np.dot(p1_0, p1 - (p2 + p0) / 2) < 0:
+        p1_0 = -p1_0
 
-        if np.dot(s, p3) < 0:
-            s = -s
+    eta = geometry.angle(p1 - (p2 + p0) / 2, p1_0)
+    if np.dot(np.cross(p1_0, p1 - (p2 + p0) / 2), p2 - p0) < 0:
+        eta = -eta
 
-    # Get a new frame
-
-    x = geometry.normalize(p1 - (p2 + p0) / 2)
-    y = np.cross(s, x)
-    z = s
-
-    # Get the ideal parameters
-
-    alpha = np.arctan2(np.dot(z, p2 - p0), np.dot(y, p2 - p0))
-    v1 = p1 - p0
-    v2 = p3 - p2
-    delta = geometry.angle(v1 - np.dot(v1, z) * z, v2 - np.dot(v2, z) * z)
-    R = np.absolute(D_MEAN * np.sin(theta1 / 2) * np.cos(alpha) / np.sin(delta / 2))
-
-    return R, alpha, delta
+    return R, delta, alpha, eta
 
 def generate_ideal_beta_sheet_from_internal_coordinates(theta1, tau1, theta2, length, num_strands):
     '''Generate an ideal beta sheet from three internal coordinates, the length of each strand
     and the number of strands.
     '''
-    # Calculate the internal coordinates
-    
-    R, alpha, delta = get_ideal_parameters_from_three_internal_coordinates(theta1, tau1, theta2)
-    theta1, tau1, theta2, tau2 = get_internal_coordinates_for_ideal_sheet(R, alpha, delta)
-
-    ds = [D_MEAN] * (length - 1)
-    thetas = ([theta1, theta2] * length)[:length - 2]
-    taus = ([tau1, tau2] * length)[:length - 3]
-
-    # Generate one strand
-
-    strand = basic.generate_segment_from_internal_coordinates(ds, thetas, taus)
-
-    # Get the outer cylinder radius
-
-    R_out = R + D_MEAN * np.cos(theta1 / 2)
-
-    # Get the screw axis
-
-    x = geometry.normalize(strand[1] - (strand[2] + strand[0]) / 2)
-    rot_x = geometry.rotation_matrix_from_axis_and_angle(x, np.pi / 2 - alpha)
-    z = geometry.normalize(np.dot(rot_x, strand[2] - strand[0]))
-    u = strand[1] - R_out * x
-
-    # Get the pitch
-
-    pitch = geometry.pitch_angle_to_pitch(alpha, R_out)
-
-    # Get the screw angle
-
-    screw_angle = -PARA_BP_LEN_PLUS * np.sin(alpha) / R_out 
-
-    # Get the screw rotation and translation
-
-    M, t = geometry.get_screw(-z, screw_angle, pitch, u)
-
-    # If the alpha is zero, do a pure translation
-
-    if np.absolute(alpha) < 0.001:
-        M = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        t = -PARA_BP_LEN_PLUS * z
-
-    # Generate strands
-
-    sheet = [strand]
-    
-    for i in range(1, num_strands):
-        sheet.append([np.dot(M, ca) + t for ca in sheet[i - 1]])
-
-    return sheet
+    pass
+#    # Calculate the internal coordinates
+#    
+#    R, alpha, delta = get_ideal_parameters_from_three_internal_coordinates(theta1, tau1, theta2)
+#    theta1, tau1, theta2, tau2 = get_internal_coordinates_for_ideal_sheet(R, alpha, delta)
+#
+#    ds = [D_MEAN] * (length - 1)
+#    thetas = ([theta1, theta2] * length)[:length - 2]
+#    taus = ([tau1, tau2] * length)[:length - 3]
+#
+#    # Generate one strand
+#
+#    strand = basic.generate_segment_from_internal_coordinates(ds, thetas, taus)
+#
+#    # Get the outer cylinder radius
+#
+#    R_out = R + D_MEAN * np.cos(theta1 / 2)
+#
+#    # Get the screw axis
+#
+#    x = geometry.normalize(strand[1] - (strand[2] + strand[0]) / 2)
+#    rot_x = geometry.rotation_matrix_from_axis_and_angle(x, np.pi / 2 - alpha)
+#    z = geometry.normalize(np.dot(rot_x, strand[2] - strand[0]))
+#    u = strand[1] - R_out * x
+#
+#    # Get the pitch
+#
+#    pitch = geometry.pitch_angle_to_pitch(alpha, R_out)
+#
+#    # Get the screw angle
+#
+#    screw_angle = -PARA_BP_LEN_PLUS * np.sin(alpha) / R_out 
+#
+#    # Get the screw rotation and translation
+#
+#    M, t = geometry.get_screw(-z, screw_angle, pitch, u)
+#
+#    # If the alpha is zero, do a pure translation
+#
+#    if np.absolute(alpha) < 0.001:
+#        M = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+#        t = -PARA_BP_LEN_PLUS * z
+#
+#    # Generate strands
+#
+#    sheet = [strand]
+#    
+#    for i in range(1, num_strands):
+#        sheet.append([np.dot(M, ca) + t for ca in sheet[i - 1]])
+#
+#    return sheet
 
 def get_strand_parameters(strand_type):
     '''Return length, angle and torsion parameters of a strand'''
--- tests/test_beta_sheet.py
+++ tests/test_beta_sheet.py
@@ -13,23 +13,25 @@ def test_build_beta_sheet():
     #theta1 = np.radians(123.9 - 10)
     #tau1 = np.radians(165)
     #tau1 = np.radians(180)
-    tau1 = np.radians(185)
+    tau1 = np.radians(195)
     #tau1 = np.radians(195.8)
     theta2 = np.radians(123.9 + 10)
+    tau2 = np.radians(185)
 
-    R, alpha, delta = ssg.beta_sheet.get_ideal_parameters_from_three_internal_coordinates(theta1, tau1, theta2)
+    R, delta, alpha, eta = ssg.beta_sheet.get_ideal_parameters_from_internal_coordinates(theta1, tau1, theta2, tau2)
+    
     theta1_calc, tau1_calc, theta2_calc, tau2_calc = \
-            ssg.beta_sheet.get_internal_coordinates_for_ideal_sheet(R, alpha, delta)
+            ssg.beta_sheet.get_internal_coordinates_for_ideal_strand(R, delta, alpha, eta)
 
-    print("R = {0:.2f}, alpha = {1:.2f}, delta = {2:.2f}".format(R, np.degrees(alpha), np.degrees(delta)))
+    print("R = {0:.2f}, alpha = {1:.2f}, delta = {2:.2f}, eta={3:.2f}".format(R, np.degrees(alpha), np.degrees(delta), np.degrees(eta)))
     print("theta1 = {0:.2f}, theta1_calc = {1:.2f}".format(np.degrees(theta1), np.degrees(theta1_calc)))
     print("tau1 = {0:.2f}, tau1_calc = {1:.2f}".format(np.degrees(tau1), np.degrees(tau1_calc)))
     print("theta2 = {0:.2f}, theta2_calc = {1:.2f}".format(np.degrees(theta2), np.degrees(theta2_calc)))
-    print("tau2_calc = {0:.2f}".format(np.degrees(tau2_calc)))
+    print("tau2 = {0:.2f}, tau2_calc = {1:.2f}".format(np.degrees(tau2), np.degrees(tau2_calc)))
 
-    
-    ca_list = ssg.beta_sheet.generate_ideal_beta_sheet_from_internal_coordinates(theta1, tau1, theta2, 20, 8)
-    ssg.IO.save_ca_list(ca_list, "ideal_sheet.pdb")
+    #
+    #ca_list = ssg.beta_sheet.generate_ideal_beta_sheet_from_internal_coordinates(theta1, tau1, theta2, 20, 8)
+    #ssg.IO.save_ca_list(ca_list, "ideal_sheet.pdb")
 
 def test_purterb_beta_sheet():
     print("test purterb beta sheet.")
@@ -38,13 +40,13 @@ def test_purterb_beta_sheet():
     tau1 = np.radians(185)
     theta2 = np.radians(123.9 + 5)
     
-    ca_list_before_purterb = ssg.beta_sheet.generate_ideal_beta_sheet_from_internal_coordinates(theta1, tau1, theta2, 10, 5)
-    ssg.IO.save_ca_list(ca_list_before_purterb, "sheet_before_purterb.pdb")
+    #ca_list_before_purterb = ssg.beta_sheet.generate_ideal_beta_sheet_from_internal_coordinates(theta1, tau1, theta2, 10, 5)
+    #ssg.IO.save_ca_list(ca_list_before_purterb, "sheet_before_purterb.pdb")
 
-    rand_strand = ssg.beta_sheet.build_a_random_strand_from_a_reference(ca_list_before_purterb[1], 'antiparallel', '-')
-    while rand_strand is None:
-        rand_strand = ssg.beta_sheet.build_a_random_strand_from_a_reference(ca_list_before_purterb[1], 'antiparallel', '-', seed=ca_list_before_purterb[0][:3])
-    ssg.IO.save_ca_list(rand_strand, 'rand_strand.pdb')
+    #rand_strand = ssg.beta_sheet.build_a_random_strand_from_a_reference(ca_list_before_purterb[1], 'antiparallel', '-')
+    #while rand_strand is None:
+    #    rand_strand = ssg.beta_sheet.build_a_random_strand_from_a_reference(ca_list_before_purterb[1], 'antiparallel', '-', seed=ca_list_before_purterb[0][:3])
+    #ssg.IO.save_ca_list(rand_strand, 'rand_strand.pdb')
 
     #rand_strand = ssg.beta_sheet.build_a_random_strand_from_a_reference(ca_list_before_purterb[1], 'antiparallel', '+')
     #while rand_strand is None:
