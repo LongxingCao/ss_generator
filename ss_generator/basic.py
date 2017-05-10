a55af8ca031b90ec9eeef40d88356274296b7819
xingjiepan <xingjiepan@gmail.com>
2017-05-10 15:42:58 -0700

    Implement change_torsions()

--- ss_generator/basic.py
+++ ss_generator/basic.py
@@ -19,3 +19,46 @@ def transform_residue_list(res_list, M, t):
     translation t. Return the new list.
     '''
     return [transform_residue(res, M, t) for res in res_list]
+
+def change_torsions(strand, res_id, phi, psi):
+    '''Change the phi, psi angles of a residue in
+    a strand.
+    '''
+
+    # Rotate the psi torsion
+
+    if res_id < len(strand) - 1:
+    
+        psi_old = geometry.dihedral(strand[res_id]['n'], strand[res_id]['ca'], 
+                strand[res_id]['c'], strand[res_id + 1]['n'])
+
+        # Get the rotation matrix
+
+        axis = strand[res_id]['c'] - strand[res_id]['ca']
+        M = geometry.rotation_matrix_from_axis_and_angle(axis, psi - psi_old)
+        t = strand[res_id]['ca'] - np.dot(M, strand[res_id]['ca'])
+
+        # Rotate subsequent atoms
+
+        strand[res_id]['o'] = np.dot(M, strand[res_id]['o']) + t
+
+        for i in range(res_id + 1, len(strand)):
+            strand[i] = transform_residue(strand[i], M, t)
+
+    # Rotate the phi torsion
+    
+    if res_id > 0:
+
+        phi_old = geometry.dihedral(strand[res_id - 1]['c'], strand[res_id]['n'],
+                strand[res_id]['ca'], strand[res_id]['c'])
+
+        # Get the rotation matrix
+
+        axis = strand[res_id]['ca'] - strand[res_id]['n']
+        M = geometry.rotation_matrix_from_axis_and_angle(axis, phi - phi_old)
+        t = strand[res_id]['ca'] - np.dot(M, strand[res_id]['ca'])
+
+        # Rotate subsequent residues
+
+        for i in range(res_id, len(strand)):
+            strand[i] = transform_residue(strand[i], M, t)
--- tests/test_beta_sheet.py
+++ tests/test_beta_sheet.py
@@ -10,6 +10,8 @@ import ss_generator as ssg
 def test_build_beta_sheet():
     print("test build beta sheet.")
     
-    res_list = ssg.beta_sheet.build_ideal_flat_beta_sheet('antiparallel', 10, 5)
+    res_list = ssg.beta_sheet.build_ideal_flat_beta_sheet('parallel', 10, 5)
     ssg.IO.save_residue_list(res_list, "ideal_flat_sheet.pdb")
 
+    ssg.basic.change_torsions(res_list[1], 5, np.radians(-120), np.radians(120))
+    ssg.IO.save_residue_list(res_list, "perturbed_sheet.pdb")
